34ae900177488a79a419f0b63212691107bfa824
Ali-Nassrallah <alinassrallah2004@gmail.com>
2023-06-28 18:41:28 +0300

    First Copy

---
+++ Nassrallah_Product/Controllers/ProductsController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nassrallah_Product.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private static List<Product> products = new List<Product>();
+        [HttpGet("GetProducts")]
+        public IEnumerable<Product> GetProducts()
+        {
+            return products;
+        }
+        [HttpGet("GetById")]
+        public Product GetById(int id)
+        {
+            var productFind = products.Find(p =>  p.Id == id);
+                return productFind;
+        }
+
+        [HttpPost("AddProduct")]
+        public void PostProduct(string name,string description,int price,int quantity)
+        {
+            Product p=new Product(name,description,price,quantity);
+
+            p.Id = new Random().Next(1000, 9999);
+            products.Add(p);
+        }
+        [HttpPut("UpdateProduct")]
+        public IActionResult UpdateProduct(int id,string newName,string newDescription,int price,int quantity)
+        {
+            var productFind = products.Find(p => p.Id == id);
+            if (productFind != null)
+            {
+                productFind.Id= new Random().Next(1000, 9999); ;
+                productFind.Name = newName;
+                productFind.Description = newDescription;
+                productFind.Price = price;
+                productFind.Quantity = quantity;
+                return Ok();
+            }
+            else
+               return NotFound();
+
+        }
+
+        [HttpDelete("DeleteProduct")]
+        public IActionResult DeleteProduct(int id)
+        {
+            var productFind = products.Find(p => p.Id == id);
+            if (productFind != null)
+            {
+                products.Remove(productFind);
+                return Ok();
+            }
+                
+            else
+                return NotFound();
+        }
+    }
+}
---
+++ Nassrallah_Product/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace Nassrallah_Product
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string name, string description, int price, int quantity)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+            Quantity = quantity;
+        }
+    }
+}
